ec390bf8f75af94fd118b4cff72f0d55538c4094
Anna Fernandes <anna.cbf@gmail.com>
2020-08-04 16:26:38 -0300

    PR aberto para entrega (não mergear)

--- 4eddit/src/components/Cadastro/Cadastro.js
+++ 4eddit/src/components/Cadastro/Cadastro.js
@@ -12,7 +12,6 @@ const initialState = {
   status: 'UPDATE_VALUE'
 }
 
-
 const formReducer = (state, action) =>{
   switch(action.type) {
     case 'UPDATE_VALUE':
@@ -23,7 +22,6 @@ const formReducer = (state, action) =>{
         return state
   }
 }
-
 
 const Cadastro = () => {
     const [state, dispatch] = useReducer(formReducer, initialState)
