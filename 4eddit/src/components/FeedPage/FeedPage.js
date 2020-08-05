36bfc4c479760a6962537dce20fb5143c4c7f4e6
Anna Fernandes <anna.cbf@gmail.com>
2020-08-05 13:03:47 -0300

    Coloquei o username no feed, pq estava fora.

--- 4eddit/src/components/FeedPage/FeedPage.js
+++ 4eddit/src/components/FeedPage/FeedPage.js
@@ -133,7 +133,8 @@ const FeedPage = () => {
                         <VoteBtn><ArrowDown /></VoteBtn>
                     </VoteBtnContainer>
                     <PostText onClick={() => goToPost(post.id)}>
-                        <h3>{post.title}</h3>
+                        <h4>{post.username}</h4>
+                        <h2>{post.title}</h2>
                         <p>{post.text}</p>
                         <p>{post.commentsCount} comentários</p>
                     </PostText>
