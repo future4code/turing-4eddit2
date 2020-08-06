5ed98ba36ccbe03df0076b87db01515f390fb1a8
Anna Fernandes <anna.cbf@gmail.com>
2020-08-06 09:52:12 -0300

    Inseri comentários nos arquivos de teste

--- 4eddit/src/App.test.js
+++ 4eddit/src/App.test.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 import userEvent from '@testing-library/user-event';
 import App from './App';
 
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlBsSFBLZkV1ZmJnc2duRzVnU1lNIiwidXNlcm5hbWUiOiJhbm5hIiwiZW1haWwiOiJhbm5hLmNiZkBnbWFpbC5jb20iLCJpYXQiOjE1OTY0NzQ0OTl9.43TaZ25rWa5I2eHYeJiAGlYf8lWR_6jga-BLIFCk4ug";
 
 const axiosConfig = {
   headers: {
@@ -29,15 +30,19 @@ const mockData = {
 // Get Posts
 axios.get = jest.fn().mockResolvedValue({data:{"posts":[]}})
 // Get Post Detail
-axios.get = jest.fn().mockResolvedValue({"posts": { }})
+axios.get = jest.fn().mockResolvedValue({data: {"posts": { }}})
 // Post Login
 axios.post = jest.fn().mockResolvedValue({});
 axios.put = jest.fn().mockResolvedValue();
 
 test('renderiza o login', () => {
+  
+  // Faz a requisição com um post mocado, para aparecer na tela
   axios.get = jest.fn().mockResolvedValue({
     data: { "posts": [ mockData ] } 
   });
+  
+  // renderiza o componente pela rota
   const history = createMemoryHistory()
   const { getByText } = render(
     <Router history={history}>
@@ -45,15 +50,18 @@ test('renderiza o login', () => {
     </Router>
   )
 
+  // verifica se a palavra login aparece na tela, para se assegurar de que a tela não está em branco (ela deve estar no botão do header)
   expect(getByText(/login/)).toBeInTheDocument();
 });
 
 test('Testa se após o usário se logar aparece o feed', async() => {
-
+  
+  // Faz a requisição com um post mocado, para aparecer na tela
   axios.get = jest.fn().mockResolvedValue({
     data: { "posts": [ mockData ] } 
   });
-
+  
+  // renderiza o componente pela rota
   const history = createMemoryHistory()
   const { getByText, getByPlaceholderText } = render(
     <Router history={history}>
@@ -61,21 +69,36 @@ test('Testa se após o usário se logar aparece o feed', async() => {
     </Router>
   )
   
-  expect(getByText(/login/)).toBeInTheDocument();
-  fireEvent.click(getByText(/login/i))
+  // Procura a palavra login na tela (o RegEx /palavra/i procura a palavra em letra maiúscula ou minúscula)
+  expect(getByText(/login/i)).toBeInTheDocument();
+
+  // Simula o clique no botão usando a biblioteca userEvent (deve ser instalada)
+  userEvent.click(getByText(/login/i))
   
+  // Verifica se apareceu a tela de login procurando o texto "Bem vindo..."
   expect(getByText(/Bem vindo ao 4eddit/)).toBeInTheDocument();
   
+  // Cria const para guardar o input que tem placeholder "Digite seu email..."
   const userInput = getByPlaceholderText(/Digite seu email aqui/i);
+  // Espera o usuário digitar (await) e guarda o valor do input
   await userEvent.type(userInput, 'bananinha@hotmail.com')
   
+  // Cria const para guardar o input que tem placeholder "Digite sua senha..."
   const passwordInput = getByPlaceholderText(/Digite sua senha aqui/i);
+  // Espera o usuário digitar (await) e guarda o valor do input
   await userEvent.type(passwordInput, '123456')
   
+  
+  // Cria const para guardar o botão que tem o texto "Entrar"
   const loginBtn = getByText(/entrar/i);
+  // Simula o clique no botão entrar
   userEvent.click(loginBtn)
   
+  // Moca a função de post, que ocorre após se clicar no botão entrar
   axios.post = jest.fn().mockResolvedValue();
+
+  // As linhas a seguir deveriam verificar se o post foi enviado com sucesso para a API com os dados guardados dos inputs, simulados acima, mas deu timeout. Em seguida, verifica se o usuário foi redirecionado para a página de feed, procurando um dos textos que existem nela.
+
   // await wait(() => {
   //   expect(axios.post).toHaveBeenCalledWith('https://us-central1-labenu-apis.cloudfunctions.net/labEddit/login', {
   //     email: 'bananinha@hotmail.com',
--- 4eddit/src/components/FeedPage/FeedPage.test.js
+++ 4eddit/src/components/FeedPage/FeedPage.test.js
@@ -21,7 +21,7 @@ const axiosConfig = {
   }
 };
 
-// Simula um objeto existente na api
+// Simula um post existente na api
 const mockData = {
   "posts": [
     {
@@ -39,7 +39,7 @@ const mockData = {
 
 describe('O formulário cria um novo post e os posts publicados aparecem na página', () => {
   test('testa acesso ao feed - caso o usuário não esteja logado, ele deve ser redirecionado para o login', () => {
-    
+    // Simula renderização do componente, independente da rota. Link: https://testing-library.com/docs/example-react-router
     const history = createMemoryHistory()
     const { getByText } = render(
       <Router history={history}>
@@ -47,16 +47,19 @@ describe('O formulário cria um novo post e os posts publicados aparecem na pág
       </Router>
     )
 
+    // Verifica se a página login está sendo renderizada, buscando o texto "entrar", que está presente no botão
     const loginBtn = getByText(/entrar/i);
     expect(loginBtn).toBeInTheDocument();
 
+    // Aqui a ideia é descobrir se ele está procurando o token e, se não encontra, redireciona a página para o lugar certo. Ainda é preciso descobrir como fazer isso.
   })
   test('testa acesso ao feed - caso o usuário esteja logado, ele vê o feed de posts', async() => {
-
+    // Simula uma requisição do API que deve conter os dados simulados (guarados na const mockData)
     axios.get = jest.fn().mockResolvedValue({ data:
         mockData
     })
     
+    // Simula a renderização do componente independente da rota (url)
     const history = createMemoryHistory()
     const { getByPlaceholderText, getByText } = render(
       <Router history={history}>
@@ -64,50 +67,59 @@ describe('O formulário cria um novo post e os posts publicados aparecem na pág
       </Router>
     )
 
-    // Form
+    // Verifica elementos presentes no Form. Se eles aparecem na tela, a página foi renderizada de forma correta.
     
+    // Verifica se existe algum elemento com o texto "Novo Post" na tela. A diferença de estrutura entre este e os próximos é que, nos próximos, os elementos que devem ser encontrados estão guardados em uma const
+    expect(getByText('Novo Post')).toBeInTheDocument()
+
+    // Verifica se existe um input com placeholder "Escreva seu título"
     const inputTitulo = getByPlaceholderText('Escreva seu título')
     expect(inputTitulo).toBeInTheDocument();
     
+    // Verifica se existe um input com placeholder "Escreva seu texto"
     const inputTexto = getByPlaceholderText('Escreva seu texto')
     expect(inputTexto).toBeInTheDocument();
-
+    
+    // Verifica se existe algum elemento com o texto "Postar"
     const botaoPostar = getByText('Postar')
     expect(botaoPostar).toBeInTheDocument();
     
-    //Feed
-
-    expect(getByText('Novo Post')).toBeInTheDocument()
-
+    // Verifica se foi feita a requisição de dados para a API com os dados simulados e o Feed aparece. Aqui, espera que a requisição tenha sido feita com a estrutura(baseUrl, axiosConfig)
     expect(axios.get).toHaveBeenCalledWith(baseUrl, axiosConfig);
 
+    // Dá tempo para a resposta da API e busca os textos simulados em mockData, que foram enviados logo no início desse teste
     await wait(() => {
       expect(getByText('Nao quer atualizar')).toBeInTheDocument();
       expect(getByText('feliperdi')).toBeInTheDocument();
     });
   })
   test('O formulário capta os valores dos inputs e envia um novo post, que aparece na tela imediatamente', async() => {
-   
+    // Simula uma requisição do API que deve conter os dados simulados (guarados na const mockData)
     axios.get = jest.fn().mockResolvedValue({ data:
         mockData
-    })
+    });
     
+    // Simula a renderização do componente independente da rota (url)
     const history = createMemoryHistory()
     const { getByPlaceholderText, getByText } = render(
       <Router history={history}>
         <FeedPage />
       </Router>
     )
-
+    
+    // Simula a digitação de "Um texto teste" no input que tem o placeholder "Escreva seu título"
     const titleInput = getByPlaceholderText(/Escreva seu título/i);
     await userEvent.type(titleInput, 'Um texto teste')
     
+    // Simula a digitação de "Oi, eu sou um texto de teste" no input que tem o placeholder "Escreva seu texto"
     const textInput = getByPlaceholderText(/Escreva seu texto/i);
     await userEvent.type(textInput, 'Oi, eu sou um texto de teste')
-
+    
+    // Busca o elemento que tem o texto 'postar' e simula o clique nele
     const postBtn = getByText(/postar/i);
     userEvent.click(postBtn)
     
+    // Aguarda a resposta da API e verifica se os dados foram enviados dessa forma para ela. Ainda não sei por que a Authorization, nesse caso, é null
     await wait( () => {
       expect(axios.post).toHaveBeenCalledWith(baseUrl, {
         title: 'Um texto teste',
@@ -119,11 +131,14 @@ describe('O formulário cria um novo post e os posts publicados aparecem na pág
       })
     },);
     
+    // Verifica se aparece a mensagem de sucesso que deve aparecer após a execução da API, que tem o texto "Seu post foi publicado com sucesso"
     const postMesage = getByText('Seu post foi publicado com sucesso.')
     expect(postMesage).toBeInTheDocument();
 
+    // Simula uma requisição para atualizar os dados de requisição de posts da API, que, agora, deve conter o novo post
     axios.get = jest.fn().mockResolvedValue({ data:{ "posts": [ ] }
-    })
+    
+    });
 
     // erro: Timeout - Async callback was not invoked within the 5000ms timeout specified by jest.setTimeout.Timeout - Async callback was not invoked within the 5000ms timeout specified by jest.setTimeout.Error:
     // await wait(() => {
@@ -140,12 +155,12 @@ describe('O formulário cria um novo post e os posts publicados aparecem na pág
 
 describe('Interação com botões dos posts', () => {
   test('As contagens de comentários e votos do post aparecem na tela', () => {
-    
+    // Deve verificar se quando a tela é rendezida, o texto mocado em mockData com a contagem dos votos e dos comentários aparece na tela de forma correta.
   })
   test('Ao votar up, aumenta em 1 a contagem de votos', () => {
-
+    // Deve verificar se, ao clicar no botão de voto up, a contagem - "votesCount": 45 - soma 1.
   })
   test('Ao votar down, diminui em 1 a contagem de votos', () => {
-
+    // Deve verificar se, ao clicar no botão de voto down, a contagem - "votesCount": 45 - diminui 1.
   })
 })
